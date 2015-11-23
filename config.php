f35b8280df2a1a62b9db0caa5cc8a5b57587a311
jamilkhan <engrjameelkhan@gmail.com>
2015-11-23 18:34:43 +0500

    Added project configuration file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+ define('DB_HOSt', 'localhost');
+ 
+ define('DB_NAME', '');
+ 
+ define('DB_USER', '');
+ 
+ define('DB_PASS', '');
+ 
+ define('SITEURL', 'http://localhost/theme');
+
+ define('ABSPATH', dirname(__FILE__).'/');
+ 
+ echo ABSPATH;
